a3bf7b53b88784452cec2444f11349c3bb7bb5e4
Mohaiminul375 <mohaiminul375@gmail.com>
2024-12-13 10:18:26 +0600

    update url

--- src/app/register/api/route.ts
+++ src/app/register/api/route.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 export const useCreateUser = () => {
     return useMutation({
         mutationFn: async (user_info: object) => {
-            const { data } = await axios.post(`http://localhost:5000/register`, user_info);
+            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/register`, user_info);
             return data;
         },
         mutationKey: ['register']
